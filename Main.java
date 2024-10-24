d502a24c5ac8785cddd13e404924e06ed92a4480
unknown <907741673@qq.com>
2024-10-25 07:06:33 +0800

    test

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * 测试一下你的Java运行环境
+ * cd 切换到此目录下
+ * javac Main.java
+ * java Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
